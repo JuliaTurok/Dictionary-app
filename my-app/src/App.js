b5ac1197fae4e4040f5e7d0965168d9ec50dac09
JuliaTurok <107355921+JuliaTurok@users.noreply.github.com>
2022-10-04 17:36:53 +0300

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/" className = "btn btn-primary">Hello</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
