13ff5b5eb2bb41d2b1d9c0f6f369187ae6a1232d
JuliaTurok <107355921+JuliaTurok@users.noreply.github.com>
2022-10-04 17:43:41 +0300

    added button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a href="/" className = "btn btn-primary">Hello</a>
+        <a href="/" className="btn btn-primary">
+          Hello
+        </a>
+        <a href="/" className="btn btn-primary mt-3">
+          Everyone
+        </a>
       </header>
     </div>
   );
