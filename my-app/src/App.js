8c8f95d171d6d77580170d52f565d5ce6264621f
JuliaTurok <107355921+JuliaTurok@users.noreply.github.com>
2022-10-04 18:36:43 +0300

    Added form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,11 @@
-
+import Dictionary from "./Dictionary";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a href="/" className="btn btn-primary">
-          Hello
-        </a>
-        <a href="/" className="btn btn-primary mt-3">
-          Everyone
-        </a>
+        <Dictionary />
       </header>
     </div>
   );
